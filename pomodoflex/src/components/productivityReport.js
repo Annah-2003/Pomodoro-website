b2aff39122dda72b5899b02b4957b2891107ea2d
Annah-2003 <irene.gitau@strathmore.edu>
2024-06-12 04:15:07 -0700

    updated productivity report component and css

---
+++ pomodoflex/src/components/productivityReport.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Container, Table } from 'react-bootstrap';
+import './ProductivityReport.css';
+
+const ProductivityReport = ({ tasks }) => {
+  const completedTasks = tasks.filter(task => task.completed).length;
+  const totalTasks = tasks.length;
+
+  return (
+    <Container className="productivity-report">
+      <h2>Productivity Report</h2>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Total Tasks</th>
+            <th>Completed Tasks</th>
+            <th>Completion Rate</th>
+          </tr>
+        </thead>
+        <tbody>
+        <tr>
+          <td>{totalTasks}</td>
+          <td>{completedTasks}</td>
+          <td>{((completedTasks / totalTasks) * 100).toFixed(2)}%</td>
+        </tr>
+      </tbody>
+    </Table>
+  </Container>
+);
+};
+
+export default ProductivityReport;
